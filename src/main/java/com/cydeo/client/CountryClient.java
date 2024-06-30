f2da6d2360ca183dee5fbae544f20fe73b719acd
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-06-30 14:08:50 -0700

    lab-10

--- src/main/java/com/cydeo/client/CountryClient.java
+++ src/main/java/com/cydeo/client/CountryClient.java
@@ -1,15 +1,19 @@
 package com.cydeo.client;
 
+import com.cydeo.dto.country.CountryResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "https://restcountires.com/v3.1", name = "COUNTRY-CLIENT")
+import java.util.List;
+
+@FeignClient(url = "https://restcountries.com/v3.1",name = "COUNTRY-CLIENT")
 public interface CountryClient {
 
+//    https://restcountries.com/v3.1 -->base url
+//    /name -->endpoint
+//    /france ->path
+
     @GetMapping("/name/{countryName}")
-    Object getCountryInfo(@PathVariable("countryName") String countryName);
-
-
-
+    List<CountryResponse> getCountryInfo(@PathVariable("countryName") String countryName);
 }
