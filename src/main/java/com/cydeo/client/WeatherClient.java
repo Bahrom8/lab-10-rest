609b64b555fb1ced6788d81a71c2ad1ed9db7799
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-06-30 13:01:00 -0700

    lab-10

--- src/main/java/com/cydeo/client/WeatherClient.java
+++ src/main/java/com/cydeo/client/WeatherClient.java
@@ -1,5 +1,6 @@
 package com.cydeo.client;
 
+import com.cydeo.dto.weather.WeatherResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(url = "http://api.weatherstack.com", name = "WEATHER-CLIENT")
 public interface WeatherClient {
 
+    //http://api.weatherstack.com -->baseUrl
+    // /current ->endpoint
+    // ?
+    // access_key=87e785a9468054d73eaf989af1e4a99a ->query param 1
+    // &
+    // query=Paris -> query param 2
+
     @GetMapping("/current")
-    Object getCurrentWeather(@RequestParam("access_key") String accessKey,
-                             @RequestParam("query") String ciy);
+    WeatherResponse getCurrentWeather(@RequestParam("access_key") String accessKey,
+                                      @RequestParam("query") String ciy);
 
 
 
